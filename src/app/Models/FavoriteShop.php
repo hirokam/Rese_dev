51a7c4bdb289a3201ca51108bdf10485cdf9057e
hirokam <h.kameyama0329@gmail.com>
2024-02-21 16:44:25 +0900

    多対多リレーション

--- src/app/Models/FavoriteShop.php
+++ src/app/Models/FavoriteShop.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class FavoriteShop extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(user::class);
+    }
+
+    public function shop()
+    {
+        return $this->belongsTo(shop::class);
+    }
 }
