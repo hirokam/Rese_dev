7eb785e363275d85830a7985b888c857c708468d
hirokam <h.kameyama0329@gmail.com>
2024-04-06 17:25:12 +0900

    responsive design/shop_detail and reservation_update

--- src/resources/views/reservation_update.blade.php
+++ src/resources/views/reservation_update.blade.php
@@ -10,7 +10,7 @@
 
 </head>
 <body>
-    <div class="grid__parent">
+    <div class="all">
         <header class="header-group">
             <div class="header__menu">
                 <form action="/menu" method="get">
@@ -23,85 +23,87 @@
                 <span class="app-title">Rese</span>
             </div>
         </header>
-        <div class="shop-detail">
-            <div class="shop-name__group">
-                <div class="return">
-                    <a href="/mypage">
-                        <button class="return__button">
-                            <span class="material-symbols-outlined">chevron_left</span>
-                        </button>
-                    </a>
+        <div class="content">
+            <div class="shop-detail__frame">
+                <div class="shop-name__group">
+                    <div class="return">
+                        <a href="/mypage">
+                            <button class="return__button">
+                                <span class="material-symbols-outlined">chevron_left</span>
+                            </button>
+                        </a>
+                    </div>
+                    <div class="shop-name__frame">
+                        <h2 class="shop-name">{{ $reservation->shop->shop_name }}</h2>
+                    </div>
                 </div>
-                <div class="shop-name__frame">
-                    <h2 class="shop-name">{{ $reservation->shop->shop_name }}</h2>
+                <img src="{{$reservation->shop->picture_url}}" alt="店舗イメージ" class="shop-image" >
+                <div class="shop__area-genre">
+                    <h3 class="shop-area">#{{ $reservation->shop->area->area }}</h3>
+                    <h3 class="shop-genre">#{{ $reservation->shop->genre->genre }}</h3>
                 </div>
+                <p class="over-view">{{ $reservation->shop->overview }}</p>
             </div>
-            <img src="{{$reservation->shop->picture_url}}" alt="店舗イメージ" class="shop-image" >
-            <div class="shop__area-genre">
-                <h3 class="shop-area">#{{ $reservation->shop->area->area }}</h3>
-                <h3 class="shop-genre">#{{ $reservation->shop->genre->genre }}</h3>
-            </div>
-            <p class="over-view">{{ $reservation->shop->overview }}</p>
-        </div>
-        <div class="reservation-group">
-            <div class="reservation__frame">
-                <h2 class="current-reservation-header">現在の予約内容</h2>
-                <div class="current-reservation-info__frame"></div>
-                <div class="current-reservation-info__inner">
-                    <div class="shop-info">
-                        <h4 class="info__header">Shop</h4>
-                        <span class="current-shop">{{ $reservation->shop->shop_name }}</span>
-                    </div>
-                    <div class="date-info">
-                        <h4 class="info__header">Date</h4>
-                        <span class="current-date">{{ $reservation->reservation_date }}</span>
-                    </div>
-                    <div class="time-info">
-                        <h4 class="info__header">Time</h4>
-                        <span class="current-time">{{ $reservation->reservation_time }}</span>
-                    </div>
-                    <div class="number-info">
-                        <h4 class="info__header">Number</h4>
-                        <span class="current-number">{{ $reservation->reservation_number }}</span>
-                    </div>
-                </div>
-                <h2 class="update-reservation-header">変更後の予約内容</h2>
-                <form action="/reservation/update" method="post">
-                @method('PATCH')
-                @csrf
-                    <input type="hidden" name="id" value="{{ $reservation->id }}">
-                    <div class="date__frame">
-                        <input type="date" name="reservation_date" class="input_date" value="input">
-                    </div>
-                    <div class="time__frame">
-                        <input type="time" name="reservation_time" class="input_time">
-                    </div>
-                    <div class="people__frame">
-                        <input type="number" name="reservation_number" class="input_people">
-                    </div>
-                    <div class="update-reservation-info__frame"></div>
-                    <div class="update-reservation-info__inner">
+            <div class="reservation-group">
+                <div class="reservation__frame">
+                    <h2 class="current-reservation-header">現在の予約内容</h2>
+                    <div class="current-reservation-info__frame"></div>
+                    <div class="current-reservation-info__inner">
                         <div class="shop-info">
                             <h4 class="info__header">Shop</h4>
-                            <span class="shop">{{ $reservation->shop->shop_name }}</span>
+                            <span class="current-shop">{{ $reservation->shop->shop_name }}</span>
                         </div>
                         <div class="date-info">
                             <h4 class="info__header">Date</h4>
-                            <span class="date"></span>
+                            <span class="current-date">{{ $reservation->reservation_date }}</span>
                         </div>
                         <div class="time-info">
                             <h4 class="info__header">Time</h4>
-                            <span class="time"></span>
+                            <span class="current-time">{{ $reservation->reservation_time }}</span>
                         </div>
                         <div class="number-info">
                             <h4 class="info__header">Number</h4>
-                            <span class="number"></span>
+                            <span class="current-number">{{ $reservation->reservation_number }}</span>
                         </div>
                     </div>
-                    <div class="update-reservation-button__frame">
-                        <button class="update-reservation__button">予約内容を変更する</button>
-                    </div>
-                </form>
+                    <h2 class="update-reservation-header">変更後の予約内容</h2>
+                    <form action="/reservation/update" method="post">
+                    @method('PATCH')
+                    @csrf
+                        <input type="hidden" name="id" value="{{ $reservation->id }}">
+                        <div class="date__frame">
+                            <input type="date" name="reservation_date" class="input_date" value="input">
+                        </div>
+                        <div class="time__frame">
+                            <input type="time" name="reservation_time" class="input_time">
+                        </div>
+                        <div class="people__frame">
+                            <input type="number" name="reservation_number" class="input_people">
+                        </div>
+                        <div class="update-reservation-info__frame"></div>
+                        <div class="update-reservation-info__inner">
+                            <div class="shop-info">
+                                <h4 class="info__header">Shop</h4>
+                                <span class="shop">{{ $reservation->shop->shop_name }}</span>
+                            </div>
+                            <div class="date-info">
+                                <h4 class="info__header">Date</h4>
+                                <span class="date"></span>
+                            </div>
+                            <div class="time-info">
+                                <h4 class="info__header">Time</h4>
+                                <span class="time"></span>
+                            </div>
+                            <div class="number-info">
+                                <h4 class="info__header">Number</h4>
+                                <span class="number"></span>
+                            </div>
+                        </div>
+                        <div class="update-reservation-button__frame">
+                            <button class="update-reservation__button">予約内容を変更する</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
--- src/resources/views/shop_detail.blade.php
+++ src/resources/views/shop_detail.blade.php
@@ -10,7 +10,7 @@
 
 </head>
 <body>
-    <div class="grid__parent">
+    <div class="all">
         <header class="header-group">
             <div class="header__menu">
                 <form action="/menu" method="get">
@@ -23,64 +23,68 @@
                 <span class="app-title">Rese</span>
             </div>
         </header>
-        <div class="shop-detail">
-            <div class="shop-name__group">
-                <div class="return">
-                    <a href="/">
-                        <button class="return__button">
-                            <span class="material-symbols-outlined">chevron_left</span>
-                        </button>
-                    </a>
-                </div>
-                <div class="shop-name__frame">
-                    <h2 class="shop-name">{{ $shop_detail->shop_name }}</h2>
+        <div class="content">
+            <div class="shop-detail__frame">
+                <div class="shop-detail">
+                    <div class="shop-name__group">
+                        <div class="return">
+                            <a href="/">
+                                <button class="return__button">
+                                    <span class="material-symbols-outlined">chevron_left</span>
+                                </button>
+                            </a>
+                        </div>
+                        <div class="shop-name__frame">
+                            <h2 class="shop-name">{{ $shop_detail->shop_name }}</h2>
+                        </div>
+                    </div>
+                    <img src="{{$shop_detail->picture_url}}" alt="店舗イメージ" class="shop-image" >
+                    <div class="shop__area-genre">
+                        <h3 class="shop-area">#{{ $shop_detail->area->area }}</h3>
+                        <h3 class="shop-genre">#{{ $shop_detail->genre->genre }}</h3>
+                    </div>
+                    <p class="over-view">{{ $shop_detail->overview }}</p>
                 </div>
             </div>
-            <img src="{{$shop_detail->picture_url}}" alt="店舗イメージ" class="shop-image" >
-            <div class="shop__area-genre">
-                <h3 class="shop-area">#{{ $shop_detail->area->area }}</h3>
-                <h3 class="shop-genre">#{{ $shop_detail->genre->genre }}</h3>
-            </div>
-            <p class="over-view">{{ $shop_detail->overview }}</p>
-        </div>
-        <div class="reservation-group">
-            <div class="reservation__frame">
-                <h2 class="reservation-header">予約</h2>
-                <form action="/reservation" method="post">
-                @csrf
-                    <input type="hidden" name="shop_id" value="{{ $shop_id }}">
-                    <div class="date__frame">
-                        <input type="date" name="reservation_date" class="input_date" value="input">
-                    </div>
-                    <div class="time__frame">
-                        <input type="time" name="reservation_time" class="input_time">
-                    </div>
-                    <div class="people__frame">
-                        <input type="number" name="reservation_number" class="input_people">
-                    </div>
-                    <div class="reservation-info__frame"></div>
-                    <div class="info__inner">
-                        <div class="shop-info">
-                            <h4 class="info__header">Shop</h4>
-                            <span class="shop">{{ $shop_detail->shop_name }}</span>
+            <div class="reservation-group">
+                <div class="reservation__frame">
+                    <h2 class="reservation-header">予約</h2>
+                    <form action="/reservation" method="post" class="reservation__form">
+                    @csrf
+                        <input type="hidden" name="shop_id" value="{{ $shop_id }}">
+                        <div class="date__frame">
+                            <input type="date" name="reservation_date" class="input_date" value="input">
                         </div>
-                        <div class="date-info">
-                            <h4 class="info__header">Date</h4>
-                            <span class="date">@error('reservation_date')<span class="validation">{{ $errors->first('reservation_date') }}</span>@enderror</span>
+                        <div class="time__frame">
+                            <input type="time" name="reservation_time" class="input_time">
                         </div>
-                        <div class="time-info">
-                            <h4 class="info__header">Time</h4>
-                            <span class="time">@error('reservation_time')<span class="validation">{{ $errors->first('reservation_time') }}</span>@enderror</span>
+                        <div class="people__frame">
+                            <input type="number" name="reservation_number" class="input_people">
                         </div>
-                        <div class="number-info">
-                            <h4 class="info__header">Number</h4>
-                            <span class="number">@error('reservation_number')<span class="validation">{{ $errors->first('reservation_number') }}</span>@enderror</span>
+                        <div class="reservation-info__frame"></div>
+                        <div class="info__inner">
+                            <div class="shop-info">
+                                <h4 class="info__header">Shop</h4>
+                                <span class="shop">{{ $shop_detail->shop_name }}</span>
+                            </div>
+                            <div class="date-info">
+                                <h4 class="info__header">Date</h4>
+                                <span class="date">@error('reservation_date')<span class="validation">{{ $errors->first('reservation_date') }}</span>@enderror</span>
+                            </div>
+                            <div class="time-info">
+                                <h4 class="info__header">Time</h4>
+                                <span class="time">@error('reservation_time')<span class="validation">{{ $errors->first('reservation_time') }}</span>@enderror</span>
+                            </div>
+                            <div class="number-info">
+                                <h4 class="info__header">Number</h4>
+                                <span class="number">@error('reservation_number')<span class="validation">{{ $errors->first('reservation_number') }}</span>@enderror</span>
+                            </div>
                         </div>
-                    </div>
-                    <div class="reservation-button__frame">
-                        <button class="reservation__button">予約する</button>
-                    </div>
-                </form>
+                        <div class="reservation-button__frame">
+                            <button class="reservation__button">予約する</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
